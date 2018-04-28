7764f177e5ec398a3fa1ed3359a989af1b355fca
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 01:26:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,6 +7,6 @@ def oxford_comma(array)
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.size > 3
     last_item = array.pop()
-    return array.join(", ") + " and " + last_item
+    return array.join(", ") + ", and " + last_item
   end
 end
